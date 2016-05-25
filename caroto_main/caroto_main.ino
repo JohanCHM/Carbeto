086bf56d7b8e2a09d31ea9d0aac0824663bf40e9
JohanCHM <hansen.mendoza@gmail.com>
2016-05-25 16:49:04 -0500

    programa base
    Primer archivo para carrito

---
+++ caroto_main/caroto_main.ino
@@ -0,0 +1,15 @@
+/*
+  Caroto
+  Programa principal de funcionamiento del carrito para Aldea
+
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
