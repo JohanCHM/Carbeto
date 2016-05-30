a045d0773cb0e1137b1a9063ce47f15ec99651ff
JohanCHM <hansen.mendoza@gmail.com>
2016-05-29 19:16:32 -0500

    1er planteamiento de maq de estados
    Primer instalación de la maquina de estados acorde al archivo 2.0 del
    diagrama de funcionamiento

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -1,17 +1,66 @@
-#include <Arduino.h>
-
-/*
-  Caroto
-  Programa principal de funcionamiento del carrito para Aldea
+/*  Nombre de archivo: main.ino
+*   Proyecto: Caroto
+*   Programa principal para el carrito de aldea
 
  */
+
+//	************************* LIBRARIES ***********************************
+#include <Arduino.h>
+
+//	************************* VARIABLES ***********************************
+enum Estados {
+  ESPERA,
+  GUARDAR,
+  DECIDIR,
+  ADELANTE,
+  IZQUIERDA,
+  DERECHA,
+  ERROR,
+  LEVANTADO
+};
+
+Estados estadoCarbeto; //!< Estado actual en el que se encuentra el robot
+
+//	************************* SETUP ***************************************
 
 void setup() {
   // put your setup code here, to run once:
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//	************************* PROGRAM ***************************************
 
-}
+void loop() {
+  switch (estadoCarbeto) {
+    case ESPERA:
+      break;
+
+    case GUARDAR:
+      break;
+
+    case DECIDIR:
+      break;
+
+    case ADELANTE:
+      break;
+
+    case IZQUIERDA:
+      break;
+
+    case DERECHA:
+      break;
+
+    case ERROR:
+      break;
+
+    case LEVANTADO:
+      break;
+
+    default:
+      //Nunca debe de llegar aquí
+      //Lanzar un mensaje de error de HW y ver si SW
+      break;
+
+  } //End switch Maq estados
+
+} //End Loop
